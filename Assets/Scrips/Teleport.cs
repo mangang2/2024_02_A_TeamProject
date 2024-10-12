b2808dfc87b01c110a1f353da453f47da2f10266
mangang2 <qkqxhddl5206@gmail.com>
2024-10-12 16:35:34 +0900

    [Feat] 순간이동 구현

---
+++ Assets/Scrips/Teleport.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject TeleportTo;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Teleporter"))
+        {
+            Player.transform.position = TeleportTo.transform.position;
+
+        }
+    }
+}
