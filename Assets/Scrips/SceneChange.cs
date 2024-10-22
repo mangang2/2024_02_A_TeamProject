d0a854f40b6842cd386652e332705da93e6839e9
mangang2 <qkqxhddl5206@gmail.com>
2024-10-22 15:34:50 +0900

    [Feat] Fade 효과 추가

--- Assets/Scrips/SceneChange.cs
+++ Assets/Scrips/SceneChange.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    public void Change()
+    public UIManager uiManager;  
+    public void SceneChanges()
     {
-        SceneManager.LoadScene("Main");
+        uiManager.FadeAndLoadScene("Main");  
     }
 
 }
+
