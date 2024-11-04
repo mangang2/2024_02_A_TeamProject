cd9ada982c66ab902b6c067f01ad2055646da524
mangang2 <qkqxhddl5206@gmail.com>
2024-11-04 23:36:47 +0900

    [Fix] 맵 수정 및 버튼 수정

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class SceneChange : MonoBehaviour
 {
-    public UIManager uiManager;  
+    public UIManager uiManager;
+    private bool hasClicked = false;  // 버튼이 눌렸는지 여부를 저장하는 변수
+
     public void SceneChanges()
     {
-        uiManager.FadeAndLoadScene("Main");  
-    }
+        if (hasClicked) return;  // 이미 버튼이 눌렸다면 아무 동작도 하지 않음
 
+        hasClicked = true;  // 버튼이 눌렸음을 기록
+        uiManager.FadeAndLoadScene("Main");  // 씬 전환 실행
+    }
 }
 
